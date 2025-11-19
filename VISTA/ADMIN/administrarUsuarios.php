cfd7dd0c681b0bf6b8afce83c92c5264a8e524b4
lianIvanSinchez <maquiavelic1030@gmail.com>
2025-11-19 15:16:26 -0300

    test de arreglo controlIngresoAdmin

--- VISTA/ADMIN/administrarUsuarios.php
+++ VISTA/ADMIN/administrarUsuarios.php
@@ -13,7 +13,9 @@ $titulo = 'Administración de Usuarios';
 
 $abmUsuario = new AbmUsuario();
 $listadoUsuarios = $abmUsuario->buscar(null);
+$sesionAdministrarUsuarios = new Session();
 
+// print_r($sesionAdministrarUsuarios->getUsRoles()[0]->getObjRol()->getIdRol());
 
 ?>
 
@@ -85,38 +87,50 @@ $listadoUsuarios = $abmUsuario->buscar(null);
                                 <td scope='row'><?php echo $estado ?></td>
 
                                 <?php
-                                if ($id == $sesion->getIdUsuario()) { ?>
+                                if ($id == $sesionAdministrarUsuarios->getIdUsuario()) { ?>
                                     <td scope='row' class='text-center'></td>
                                     <td scope='row' class='text-center'></td>
                                     <td scope='row' class='text-center'></td>
                             </tr>
                         <?php
-                                } else { ?>
+                        } else { ?>
+                        <?php
+                            if($sesionAdministrarUsuarios->getUsRoles()[0]->getObjRol()->getIdRol() === 1){
+                        ?>    
                             <form method='post' action='actualizarUsuarios.php'>
                                 <td class='text-center'>
                                     <input name='idusuario' id='idusuario' type='hidden' value='<?php echo $id ?>'>
                                     <button class='btn btn-warning btn-sm' type='submit' role='button'><i class='bi bi-pencil-square'></i>&nbsp;Editar</button>
                                 </td>
                             </form>
-
-                            <form method='post' action='eliminarUsuario.php'>
-                                <td class='text-center'>
-                                    <input name='idusuario' id='idusuario' type='hidden' value='<?php echo $id ?>'>
-                                    <button class='btn btn-danger btn-sm' type='submit' value='<?php $id ?>' role='button'><i class='bi bi-trash'></i>&nbsp;Eliminar</button>
-                                </td>
-                            </form>
-
+                        <?php
+                            }
+                        ?>
+                            <?php
+                            if($sesionAdministrarUsuarios->getUsRoles()[0]->getObjRol()->getIdRol() === 1){
+                            ?>
+                                <form method='post' action='eliminarUsuario.php'>
+                                    <td class='text-center'>
+                                        <input name='idusuario' id='idusuario' type='hidden' value='<?php echo $id ?>'>
+                                        <button class='btn btn-danger btn-sm' type='submit' value='<?php $id ?>' role='button'><i class='bi bi-trash'></i>&nbsp;Eliminar</button>
+                                    </td>
+                                </form>
+                            <?php
+                            }
+                            ?>
 
                             <form method='post' action='deshabilitarUsuario.php'>
                                 <td class='text-center'>
                                     <input name='idusuario' id='idusuario' type='hidden' value='<?php echo $id ?>'>
                                     <?php
-                                    if ($usuario->getUsdeshabilitado() == "0000-00-00 00:00:00") { ?>
-                                        <button class='btn btn-secondary btn-sm' type='submit' value='<?php $id ?>' role='button'><i class='fas fa-ban'></i>&nbsp;Deshabilitar</button>
-                                    <?php
-                                    } else { ?>
-                                        <button class='btn btn-success btn-sm' type='submit' value='<?php $id ?>' role='button'><i class="fas fa-check-circle"></i>&nbsp;Habilitar</button>
-                                    <?php
+                                    if($sesionAdministrarUsuarios->getUsRoles()[0]->getObjRol()->getIdRol() === 1){ 
+                                        if ($usuario->getUsdeshabilitado() == "0000-00-00 00:00:00") { ?>
+                                            <button class='btn btn-secondary btn-sm' type='submit' value='<?php $id ?>' role='button'><i class='fas fa-ban'></i>&nbsp;Deshabilitar</button>
+                                        <?php
+                                        } else { ?>
+                                            <button class='btn btn-success btn-sm' type='submit' value='<?php $id ?>' role='button'><i class="fas fa-check-circle"></i>&nbsp;Habilitar</button>
+                                        <?php
+                                        }
                                     }
                                     ?>
                                 </td>
