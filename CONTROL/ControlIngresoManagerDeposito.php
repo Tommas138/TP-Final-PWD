4ea2b2a4f16952c49825cc6cac05e18775f69681
Tomas Mengon <tomaso200208@gmail.com>
2025-11-20 17:33:23 -0300

    fix2

--- CONTROL/ControlIngresoManagerDeposito.php
+++ CONTROL/ControlIngresoManagerDeposito.php
@@ -10,17 +10,15 @@ Class ControlIngresoManagerDeposito {
             exit;
         }
 
-        if ($session->getUsRoles()[0]->getObjRol()->getIdRol() != 2 ) {
+        if ($session->getUsRoles()[0]->getObjRol()->getIdRol() != 2 || $session->getUsRoles()[0]->getObjRol()->getIdRol() != 1 ) {
             if (isset($session->getUsRoles()[1])) {
-                if ($session->getUsRoles()[1]->getObjRol()->getIdRol() != 2) {
+                if ($session->getUsRoles()[1]->getObjRol()->getIdRol() != 2 || $session->getUsRoles()[0]->getObjRol()->getIdRol() != 1) {
                     header('Location: ../home/index.php');
                     exit;
                 }
                 header('Location: ../managerDeposito/' . $pagina . '.php?verificado=1');
                 exit;
             }
-           header('Location: ../home/index.php');
-                    exit;
         }
     }
 }
