ab0e8d1ad27418c45fe7485b80a6c94e26322c14
Tomas Mengon <tomaso200208@gmail.com>
2025-11-20 13:54:18 -0300

    agrego archivos

---
+++ VISTA/ACCION/ESTRUCTURA/reusables/footer.php
@@ -0,0 +1,20 @@
+<style>
+    nav {
+        position:absolute;
+    }
+    body {
+        position: relative;
+    }
+    #footer {
+        height: 100px;
+
+    }
+    #small {
+        font-size:1.2rem;
+    }
+</style>
+<nav id="footer" class="navbar navbar-expand-custom navbar-mainbg position-absolute" style="bottom:0px; width:100%;">
+
+    <small id = "small" class="navbar-brand navbar-logo navbar-button " href="#">Copyright © 2025-2025 Dunder Mifflin Store</small>
+    <small id = "small" class="navbar-brand navbar-logo navbar-button " href="#">Programacion Web Dinamica</small>
+    </nav>
---
+++ VISTA/CLIENTE/modificar.php
@@ -0,0 +1,67 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<?php
+include_once __DIR__ . '../../../UTILS/funciones.php';
+include_once '../ACCION/ESTRUCTURA/reusables/header.php';
+
+$sesion = new Session();
+
+if (!$sesion->activa()) {
+    header('Location: ../home/index.php');
+    exit;
+}
+
+$titulo = 'Actualizar Usuario';
+
+$datos = data_submitted();
+$abmUsuario = new AbmUsuario();
+$idUsuario = $sesion->getIdUsuario();
+
+$arrayBusqueda = ["idusuario" => $idUsuario];
+
+$listaUsuarios = $abmUsuario->buscar($arrayBusqueda);
+$objUsuario = $listaUsuarios[0];
+
+if (isset($listaUsuarios)) {
+    $idUsuario = $listaUsuarios[0]->getIdUsuario();
+}
+
+
+
+?>
+<div class="container mt-3">
+    <h1 class="text-center">Modificación de Usuario</h1>
+    <div class="col-md-4"></div>
+    <div class="offset-md-4">
+        <form action="../acciones/accionActualizarUsuario.php" method="post" class="col-md-6 mt-3 " id="actualizarUsuario" name="actualizarUsuario">
+            <div class="">
+                <div class="form-floating mb-3">
+                    <input class="form-control" id="usnombre" name="usnombre" type="text" placeholder="Nombre de usuario" value="<?php echo $objUsuario->getUsnombre(); ?>">
+                    <label for="usnombre">Nombre de usuario: </label>
+                </div>
+            </div>
+            <div class="">
+                <div class="form-floating mb-3">
+                    <input class="form-control" id="uspass" name="uspass" type="text" placeholder="Contraseña Nueva">
+                    <label for="uspass">Contraseña Nueva: </label>
+                </div>
+            </div>
+            <div class="">
+                <div class="form-floating mb-3">
+                    <input class="form-control" id="usmail" name="usmail" type="text" placeholder="Correo Electronico" value="<?php echo $objUsuario->getUsmail(); ?>">
+                    <label for="usmail">Correo Electronico: </label>
+                </div>
+            </div>
+
+            <input class="form-control" id="idusuario" name="idusuario" type="text" value="<?php echo $objUsuario->getIdUsuario(); ?>" hidden>
+            <div class=" mb-3">
+                <div class="d-grid">
+                    <button class="btn btn-primary mt-3" type="submit">Modificar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<?php
+include_once '../../VISTA/ACCION/ESTRUCTURA/reusables/footer.php';
+?>
